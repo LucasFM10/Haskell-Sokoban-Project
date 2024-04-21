1ce35f3775713a608205c33dc5caae5d128b0dd3
LucasFM10 <lucasfm.py@gmail.com>
2024-04-21 16:40:25 -0300

    ok

--- main.hs
+++ main.hs
@@ -8,3 +8,5 @@ main = do
     boards <- readLevels "levels.txt"  -- Supõe que este arquivo contém todos os níveis
     putStrLn "Todos os níveis foram carregados."
     showMenu ("Bem vindo ao Sokoban!\nDesenvolvido por Lucas Farias e João Victor para a cadeira de programação funcional!\nEscolha um nível para jogar digitando um número entre 1 e " ++ show (length boards) ++ " ou escreva 'quit' para encerrar o jogo.") boards
+
+    
